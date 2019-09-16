61e70637581fce40830c7915c5269c5768faae72
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-09-16 13:22:55 +0200

    190916 lesson

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RazorPagesPizza.Pages
 {
     public class AboutModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
         public string Message { get; set; }
 
         public void OnGet()
         {
             Message = "Your application description page.";
         }
+
+        public IActionResult OnPost()
+        {
+            Message += " můj dovětek";
+
+            return RedirectToPage("/Index");
+        }
+
+        public void OnGetDetail()
+        {
+            
+        }
+
+
     }
 }
